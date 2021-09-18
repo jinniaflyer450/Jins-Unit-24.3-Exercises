71c6fb6b66cc4fa4b8db62652e343002225ca7d6
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-09-18 09:42:07 -0500

    added configs and imported necessary modules to app.py and models.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template, redirect, flash, jsonify, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'catdog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ob1wankenobi@localhost/blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+db.create_all()
---
+++ models.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
