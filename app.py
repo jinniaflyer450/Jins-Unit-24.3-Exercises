4dff4c3c1e710e8c9551b64d30e904ff34e0c0d7
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-09-18 12:17:08 -0500

    added patch and delete routes successfully

--- app.py
+++ app.py
@@ -34,3 +34,20 @@ def create_cupcake():
     db.session.add(new_cupcake)
     db.session.commit()
     return (jsonify(cupcake=new_cupcake.serialize()), 201)
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    cupcake.flavor=request.json.get("flavor", cupcake.flavor)
+    cupcake.size=request.json.get("size", cupcake.size)
+    cupcake.rating=request.json.get("rating", cupcake.rating)
+    cupcake.image=request.json.get("image", cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+    cupcake=Cupcake.query.get_or_404(cupcake_id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message="deleted")
